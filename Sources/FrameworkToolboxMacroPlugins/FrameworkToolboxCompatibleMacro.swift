6260b2a6d22a57ed36f26b5f092002771b34573c
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-02-09 21:41:30 +0800

    Fix build errors

--- Sources/FrameworkToolboxMacroPlugins/FrameworkToolboxCompatibleMacro.swift
+++ Sources/FrameworkToolboxMacroPlugins/FrameworkToolboxCompatibleMacro.swift
@@ -1,7 +1,6 @@
 import Foundation
 import SwiftSyntax
 import SwiftSyntaxMacros
-import MacroToolkit
 
 public enum FrameworkToolboxCompatibleMacro: MemberMacro {
     public static func expansion(of node: AttributeSyntax, providingMembersOf declaration: some DeclGroupSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
