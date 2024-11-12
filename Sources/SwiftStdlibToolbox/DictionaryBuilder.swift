775a05609443558398211736aa60cd679b7baa6a
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-11-13 00:27:35 +0800

    Update DictionaryBuilder

--- Sources/SwiftStdlibToolbox/DictionaryBuilder.swift
+++ Sources/SwiftStdlibToolbox/DictionaryBuilder.swift
@@ -1,5 +1,7 @@
 @resultBuilder
-public enum ArrayBuilder<Element> {
+public enum DictionaryBuilder<Key: Hashable, Value> {
+    public typealias Element = (Key, Value)
+    
     public static func buildPartialBlock(first: Element) -> [Element] { [first] }
 
     public static func buildPartialBlock(first: [Element]) -> [Element] { first }
@@ -8,30 +10,23 @@ public enum ArrayBuilder<Element> {
 
     public static func buildPartialBlock(accumulated: [Element], next: [Element]) -> [Element] { accumulated + next }
 
-    /// Empty block
     public static func buildBlock() -> [Element] { [] }
 
-    /// Empty partial block. Useful for switch cases to represent no elements.
     public static func buildPartialBlock(first: Void) -> [Element] { [] }
 
-    /// Impossible partial block. Useful for fatalError().
     public static func buildPartialBlock(first: Never) -> [Element] {}
 
-    /// Block for an 'if' condition.
     public static func buildIf(_ element: [Element]?) -> [Element] { element ?? [] }
 
-    /// Block for an 'if' condition which also have an 'else' branch.
     public static func buildEither(first: [Element]) -> [Element] { first }
 
-    /// Block for the 'else' branch of an 'if' condition.
     public static func buildEither(second: [Element]) -> [Element] { second }
 
-    /// Block for an array of elements. Useful for 'for' loops.
     public static func buildArray(_ components: [[Element]]) -> [Element] { components.flatMap { $0 } }
 }
 
-extension Array {
-    public init(@ArrayBuilder<Element> _ builder: () -> [Element]) {
-        self.init(builder())
+extension Dictionary {
+    public init(@DictionaryBuilder<Key, Value> _ builder: () -> [(Key, Value)]) {
+        self.init(uniqueKeysWithValues: builder())
     }
 }
