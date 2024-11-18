032d1dab257b453b9a307649c3582609122a47e8
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-11-18 17:33:24 +0800

    Update swift-syntax from apple to swiftlang

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/apple/swift-syntax.git",
+            url: "https://github.com/swiftlang/swift-syntax.git",
             from: "509.0.0"
         )
     ],
