9ea546d17fb8fca6ff2fd67f262b8b5d8fd8f6e2
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-12-08 10:01:13 +0800

    Updates

--- Sources/FoundationToolbox/AssociatedValue.swift
+++ Sources/FoundationToolbox/AssociatedValue.swift
@@ -9,91 +9,202 @@
 
 import Foundation
 import ObjectiveC.runtime
-import SwiftStdlibToolbox
-
-private extension String {
-    var address: UnsafeRawPointer {
-        return UnsafeRawPointer(bitPattern: abs(hashValue))!
-    }
-}
+import FrameworkToolbox
 
 /**
  Returns the associated value for the specified object and key.
- 
+
  - Parameters:
     - key: The key of the associated value.
     - object: The object of the associated value.
- - Returns: The associated value for the object and key, or nil if the value couldn't be found for the key..
+ - Returns: The associated value for the object and key, or `nil` if the value couldn't be found for the key.
  */
-public func getAssociatedValue<T>(key: String, object: AnyObject) -> T? {
-    return (objc_getAssociatedObject(object, key.address) as? _AssociatedValue)?.value as? T
+public func getAssociatedValue<T>(_ key: String, object: AnyObject) -> T? {
+    (objc_getAssociatedObject(object, key.address) as? AssociatedValue)?.value as? T
 }
 
 /**
  Returns the associated value for the specified object, key and inital value.
- 
+
  - Parameters:
     - key: The key of the associated value.
     - object: The object of the associated value.
     - initialValue: The inital value of the associated value.
  - Returns: The associated value for the object and key.
  */
-public func getAssociatedValue<T>(key: String, object: AnyObject, initialValue: @autoclosure () -> T) -> T {
-    return getAssociatedValue(key: key, object: object) ?? setAndReturn(initialValue: initialValue(), key: key, object: object)
+public func getAssociatedValue<T>(_ key: String, object: AnyObject, initialValue: @autoclosure () -> T) -> T {
+    getAssociatedValue(key, object: object) ?? setAndReturn(initialValue: initialValue(), key: key, object: object)
 }
 
 /**
  Returns the associated value for the specified object, key and inital value.
- 
+
  - Parameters:
     - key: The key of the associated value.
     - object: The object of the associated value.
     - initialValue: The inital value of the associated value.
  - Returns: The associated value for the object and key.
  */
-public func getAssociatedValue<T>(key: String, object: AnyObject, initialValue: () -> T) -> T {
-    return getAssociatedValue(key: key, object: object) ?? setAndReturn(initialValue: initialValue(), key: key, object: object)
-}
-
-private func setAndReturn<T>(initialValue: T, key: String, object: AnyObject) -> T {
-    set(associatedValue: initialValue, key: key, object: object)
-    return initialValue
+public func getAssociatedValue<T>(_ key: String, object: AnyObject, initialValue: () -> T) -> T {
+    getAssociatedValue(key, object: object) ?? setAndReturn(initialValue: initialValue(), key: key, object: object)
 }
 
 /**
  Sets a associated value for the specified object and key.
- 
+
  - Parameters:
     - associatedValue: The value of the associated value.
     - key: The key of the associated value.
     - object: The object of the associated value.
  */
-public func set<T>(associatedValue: T?, key: String, object: AnyObject) {
-    set(associatedValue: _AssociatedValue(associatedValue), key: key, object: object)
+public func setAssociatedValue<T>(_ value: T?, key: String, object: AnyObject) {
+    set(associatedValue: AssociatedValue(value), key: key, object: object)
 }
 
 /**
  Sets a weak associated value for the specified object and key.
- 
+
  - Parameters:
     - weakAssociatedValue: The weak value of the associated value.
     - key: The key of the associated value.
     - object: The object of the associated value.
  */
-public func set<T: AnyObject>(weakAssociatedValue: T?, key: String, object: AnyObject) {
-    set(associatedValue: _AssociatedValue(weak: weakAssociatedValue), key: key, object: object)
+public func setAssociatedValue<T: AnyObject>(weak value: T?, key: String, object: AnyObject) {
+    set(associatedValue: AssociatedValue(weak: value), key: key, object: object)
 }
 
-private func set(associatedValue: _AssociatedValue, key: String, object: AnyObject) {
+private func set(associatedValue: AssociatedValue, key: String, object: AnyObject) {
     objc_setAssociatedObject(object, key.address, associatedValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
 }
 
-private class _AssociatedValue {
+private func setAndReturn<T>(initialValue: T, key: String, object: AnyObject) -> T {
+    setAssociatedValue( initialValue, key: key, object: object)
+    return initialValue
+}
+
+
+extension FrameworkToolbox where Base: NSObject {
+    /**
+     Returns the associated value for the specified  key.
+
+     - Parameters:
+        - key: The key of the associated value.
+     - Returns: The associated value for the key, or `nil` if the value couldn't be found for the key.
+     */
+    public func getAssociatedValue<T>(_ key: String) -> T? {
+        FoundationToolbox.getAssociatedValue(key, object: base)
+    }
+    
+    /**
+     Returns the associated value for the specified key and inital value.
+
+     - Parameters:
+        - key: The key of the associated value.
+        - initialValue: The inital value of the associated value.
+     - Returns: The associated value for the object and key.
+     */
+    public func getAssociatedValue<T>(_ key: String, initialValue: @autoclosure () -> T) -> T {
+        FoundationToolbox.getAssociatedValue(key, object: base, initialValue: initialValue)
+    }
+    
+    /**
+     Returns the associated value for the specified key and inital value.
+
+     - Parameters:
+        - key: The key of the associated value.
+        - initialValue: The inital value of the associated value.
+     - Returns: The associated value for the key.
+     */
+    public func getAssociatedValue<T>(_ key: String, initialValue: () -> T) -> T {
+        FoundationToolbox.getAssociatedValue(key, object: base, initialValue: initialValue)
+    }
+    
+    /**
+     Sets an associated value for the specified key.
+
+     - Parameters:
+        - value: The value to set.
+        - key: The key of the associated value.
+     */
+    public func setAssociatedValue<T>(_ value: T?, key: String) {
+        FoundationToolbox.setAssociatedValue(value, key: key, object: base)
+    }
+    
+    /**
+     Sets a weak associated value for the specified key.
+
+     - Parameters:
+        - value: The weak value to set.
+        - key: The key of the associated value.
+     */
+    public func setAssociatedValue<T: AnyObject>(weak value: T?, key: String) {
+        FoundationToolbox.setAssociatedValue(weak: value, key: key, object: base)
+    }
+    
+    /**
+     Returns the associated value for the specified  key.
+
+     - Parameters:
+        - key: The key of the associated value.
+     - Returns: The associated value for the key, or `nil` if the value couldn't be found for the key.
+     */
+    public static func getAssociatedValue<T>(_ key: String) -> T? {
+        FoundationToolbox.getAssociatedValue(key, object: Base.self)
+    }
+    
+    /**
+     Returns the associated value for the specified key and inital value.
+
+     - Parameters:
+        - key: The key of the associated value.
+        - initialValue: The inital value of the associated value.
+     - Returns: The associated value for the object and key.
+     */
+    public static func getAssociatedValue<T>(_ key: String, initialValue: @autoclosure () -> T) -> T {
+        FoundationToolbox.getAssociatedValue(key, object: Base.self, initialValue: initialValue)
+    }
+    
+    /**
+     Returns the associated value for the specified key and inital value.
+
+     - Parameters:
+        - key: The key of the associated value.
+        - initialValue: The inital value of the associated value.
+     - Returns: The associated value for the key.
+     */
+    public static func getAssociatedValue<T>(_ key: String, initialValue: () -> T) -> T {
+        FoundationToolbox.getAssociatedValue(key, object: Base.self, initialValue: initialValue)
+    }
+    
+    /**
+     Sets an associated value for the specified key.
+
+     - Parameters:
+        - value: The value to set.
+        - key: The key of the associated value.
+     */
+    public static func setAssociatedValue<T>(_ value: T?, key: String) {
+        FoundationToolbox.setAssociatedValue(value, key: key, object: Base.self)
+    }
+    
+    /**
+     Sets a weak associated value for the specified key.
+
+     - Parameters:
+        - value: The weak value to set.
+        - key: The key of the associated value.
+     */
+    public static func setAssociatedValue<T: AnyObject>(weak value: T?, key: String) {
+        FoundationToolbox.setAssociatedValue(weak: value, key: key, object: Base.self)
+    }
+}
+
+private class AssociatedValue {
     weak var _weakValue: AnyObject?
     var _value: Any?
 
     var value: Any? {
-        return _weakValue ?? _value
+        _weakValue ?? _value
     }
 
     init(_ value: Any?) {
@@ -105,75 +216,8 @@ private class _AssociatedValue {
     }
 }
 
-/// A type that provides associated values.
-public protocol AssociatedValuesProvider: AnyObject { }
-public extension AssociatedValuesProvider {
-    /// The associated values of the object.
-    var associatedValue: AssociatedValue<Self> {
-        AssociatedValue(self)
-    }
-}
-extension NSObject: AssociatedValuesProvider { }
-
-/// An object for getting and setting associated values of an object.
-public class AssociatedValue<Object: AssociatedValuesProvider> {
-    internal weak var object: Object!
-    internal init(_ object: Object) {
-        self.object = object
-    }
-    
-    subscript<Value>(associated: String) -> Value? {
-        get { getAssociatedValue(key: associated, object: object) }
-        set { set(associatedValue: newValue, key: associated, object: object) }
-    }
-    
-    subscript<Value>(associated: String, initialValue initialValue: () -> Value) -> Value {
-        get { getAssociatedValue(key: associated, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated, object: object) }
-    }
-    
-    subscript<Value>(associated: String, initialValue initialValue: @autoclosure () -> Value) -> Value {
-        get { getAssociatedValue(key: associated, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated, object: object) }
-    }
-    
-    subscript<Value>(associated: String, initialValue initialValue: () -> Value?) -> Value? {
-        get { getAssociatedValue(key: associated, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated, object: object) }
-    }
-    
-    subscript<Value>(associated: String, initialValue initialValue: @autoclosure () -> Value?) -> Value? {
-        get { getAssociatedValue(key: associated, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated, object: object) }
-    }
-    
-    subscript<Value>(associated: KeyPath<Object, Value>) -> Value? {
-        get { getAssociatedValue(key: associated.stringValue, object: object) }
-        set { set(associatedValue: newValue, key: associated.stringValue, object: object) }
-    }
-    
-    subscript<Value>(associated: KeyPath<Object, Value>, initialValue initialValue: () -> Value) -> Value {
-        get { getAssociatedValue(key: associated.stringValue, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated.stringValue, object: object) }
-    }
-    
-    subscript<Value>(associated: KeyPath<Object, Value>, initialValue initialValue: @autoclosure () -> Value) -> Value {
-        get { getAssociatedValue(key: associated.stringValue, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated.stringValue, object: object) }
-    }
-    
-    subscript<Value>(associated: KeyPath<Object, Value?>) -> Value? {
-        get { getAssociatedValue(key: associated.stringValue, object: object) }
-        set { set(associatedValue: newValue, key: associated.stringValue, object: object) }
-    }
-    
-    subscript<Value>(associated: KeyPath<Object, Value?>, initialValue initialValue: () -> Value?) -> Value? {
-        get { getAssociatedValue(key: associated.stringValue, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated.stringValue, object: object) }
-    }
-    
-    subscript<Value>(associated: KeyPath<Object, Value?>, initialValue initialValue: @autoclosure () -> Value?) -> Value? {
-        get { getAssociatedValue(key: associated.stringValue, object: object, initialValue: initialValue) }
-        set { set(associatedValue: newValue, key: associated.stringValue, object: object) }
+private extension String {
+    var address: UnsafeRawPointer {
+        UnsafeRawPointer(bitPattern: abs(hashValue))!
     }
 }
